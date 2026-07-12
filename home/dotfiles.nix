2fff1e205a9e40a018d32bdea5bed91df0abc83b
guuhto <gustavocorsino50@gmail.com>
2026-07-12 17:33:05 -0300

    add p10k config and dotfiles

--- home/dotfiles.nix
+++ home/dotfiles.nix
@@ -3,6 +3,7 @@
   home.file = {
     ".config/fastfetch".source = ../config/fastfetch;
     ".config/kitty".source = ../config/kitty;
+    ".p10k.zsh".source = ../p10k.zsh;
     ".config/spotatui".source = ../config/spotatui;
     ".config/nnn".source = ../config/nnn;
     ".config/zed".source = ../config/zed;
