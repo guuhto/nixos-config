ce18465167ee7f94e185cb32c10a669951c0f5e9
guuhto <gustavocorsino50@gmail.com>
2026-08-09 22:21:09 -0300

    flake: track nixpkgs unstable with home-manager master

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     spicetify-nix.url = "github:Gerg-L/spicetify-nix";
     home-manager = {
-      url = "github:nix-community/home-manager/release-26.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     plasma-manager = {
