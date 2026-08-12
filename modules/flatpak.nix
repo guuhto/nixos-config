1aa497844396788105712095d8496c5323fb16f0
guuhto <gustavocorsino50@gmail.com>
2026-08-11 22:03:38 -0300

    flatpak: don't abort activation when flathub is unreachable

--- modules/flatpak.nix
+++ modules/flatpak.nix
@@ -3,7 +3,7 @@
   services.flatpak.enable = true;
   system.activationScripts.flathub = {
     text = ''
-      ${pkgs.flatpak}/bin/flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+      ${pkgs.flatpak}/bin/flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo || true
     '';
   };
 }
